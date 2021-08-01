d89f0186057237fe790f2522d8481b4c1ba819f8
adamchipmanpenton <adamchipmanpenton@gmail.com>
2021-08-01 13:38:14 -0230

    First commint

---
+++ src/Hangman.java
@@ -0,0 +1,36 @@
+public class Hangman {
+
+
+    public String getHangManWord() {
+        return hangManWord;
+    }
+
+    public void setHangManWord(String hangManWord) {
+        this.hangManWord = hangManWord;
+    }
+
+    public String getGuessWord() {
+        return guessWord;
+    }
+
+    public void setGuessWord(String guessWord) {
+        this.guessWord = guessWord;
+    }
+
+    public String getShowHangMan() {
+        return showHangMan;
+    }
+
+    public void setShowHangMan(String showHangMan) {
+        this.showHangMan = showHangMan;
+    }
+
+    private String hangManWord;
+    private String guessWord;
+    private String showHangMan;
+
+
+
+
+
+}
