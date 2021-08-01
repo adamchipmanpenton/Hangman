e1b09498e80866a13b7f655334f6bd0ad655f070
adamchipmanpenton <adamchipmanpenton@gmail.com>
2021-08-01 14:40:58 -0230

    Second commit

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,20 +1,21 @@
+import java.util.Scanner;
+
+
 public class Hangman {
+
+
 
 
     public String getHangManWord() {
         return hangManWord;
     }
 
-    public void setHangManWord(String hangManWord) {
-        this.hangManWord = hangManWord;
+    public String getGuessLetter() {
+        return guessLetter;
     }
 
-    public String getGuessWord() {
-        return guessWord;
-    }
-
-    public void setGuessWord(String guessWord) {
-        this.guessWord = guessWord;
+    public void setGuessLetter(String guessLetter) {
+        this.guessLetter = guessLetter;
     }
 
     public String getShowHangMan() {
@@ -25,12 +26,104 @@ public class Hangman {
         this.showHangMan = showHangMan;
     }
 
-    private String hangManWord;
-    private String guessWord;
+
+    private String[] hangManWordBank = {"pizza", "hamburgers", "sandwich", "hotdogs", "spaghetti", "taco" };
+    private String hangManWord = hangManWordBank[(int) (Math.random() * hangManWordBank.length)];
+    private String guessLetter;
     private String showHangMan;
+    private int timesGuessedWrong;
 
 
+    public void findGuss(){
+        for(int i=0; i < hangManWord.length(); i++){
+            System.out.println(hangManWord.charAt(i));
+        }
+
+    }
 
 
+    public void displayHangMan(){
 
+        if (timesGuessedWrong == 1) {
+            System.out.println("Wrong guess, try again");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println("___|___");
+            System.out.println();
+        }
+        if (timesGuessedWrong == 2) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (timesGuessedWrong == 3) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   | ");
+            System.out.println("___|___");
+        }
+        if (timesGuessedWrong == 4) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (timesGuessedWrong == 5) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |");
+            System.out.println("___|___");
+        }
+        if (timesGuessedWrong == 6) {
+            System.out.println("Wrong guess, try again");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |           |");
+            System.out.println("   |           |");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+        }
+        if (timesGuessedWrong == 7) {
+            System.out.println("GAME OVER!");
+            System.out.println("   ____________");
+            System.out.println("   |          _|_");
+            System.out.println("   |         /   \\");
+            System.out.println("   |        |     |");
+            System.out.println("   |         \\_ _/");
+            System.out.println("   |          _|_");
+            System.out.println("   |         / | \\");
+            System.out.println("   |          / \\ ");
+            System.out.println("___|___      /   \\");
+            System.out.println("GAME OVER! The word was " + hangManWord);
+        }
+    }
 }
--- src/PlayHangman.java
+++ src/PlayHangman.java
@@ -1,4 +1,25 @@
+import java.security.PublicKey;
+import java.util.Scanner;
+
 public class PlayHangman {
 
-    
+
+    public static void main(String[] args) {
+        Hangman playHangMan = new Hangman();
+        Scanner sc = new Scanner(System.in);
+        String letterGuess = "";
+
+
+        System.out.println("Guess letter: ");
+        letterGuess = sc.next();
+        playHangMan.setGuessLetter(letterGuess);
+
+        System.out.println(playHangMan.getHangManWord() + "  test " + playHangMan.getGuessLetter());
+
+        playHangMan.displayHangMan();
+        playHangMan.findGuss();
+
+    }
+
+
 }
